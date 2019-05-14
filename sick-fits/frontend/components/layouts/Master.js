92b9a2d134c3a5d616b8d76d96caee92f85518ee
isjulianJaywing <julian.jackson@jaywing.com>
2019-05-14 22:19:46 +0100

    video 6 header styles

--- sick-fits/frontend/components/layouts/Master.js
+++ sick-fits/frontend/components/layouts/Master.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import Header from '../Header';
-import Meta from '../Meta';
+import React, { useContext } from "react";
+import Header from "../Header";
+import Meta from "../Meta";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
-// import PropTypes from 'prop-types';
+const theme = {
+  red: "#ff0000",
+  black: "#393939",
+  grey: "#3a3a3a",
+  lightGrey: "#e1e1e1",
+  offWhite: "ededed",
+  maxWidth: "1000px",
+  bs: "0 2px 24px 0 rgba(0,0,0,0.89)"
+};
+
+const StyledMaster = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 const Master = props => {
   return (
-    <div>
-      <Meta />
-      <Header />
-      {props.children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledMaster>
+        <Meta />
+        <Header />
+        <Inner>{props.children}</Inner>
+      </StyledMaster>
+    </ThemeProvider>
   );
 };
-
-// Master.propTypes = {};
 
 export default Master;
